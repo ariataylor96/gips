9657c93713bc04455f2f13af5faf90093f0eb8b0
Mark Rawls <markrawls96@gmail.com>
2021-10-16 19:49:24 -0400

    Fix integer parsing

--- records/loading.go
+++ records/loading.go
@@ -10,35 +10,44 @@ import (
 	"os"
 )
 
-func getXBytes(buf *bufio.Reader, n int) []byte {
-	data := make([]byte, n)
+func getXBytes(buf *bufio.Reader, n int) (data []byte, rerr error) {
+	data = make([]byte, n)
 
 	read, err := io.ReadFull(buf, data)
 	if err != nil {
-		panic(err)
+		rerr = err
+		return
 	}
 
 	if read != n {
 		panic(errors.New(fmt.Sprintf("Read fewer bytes than expected: %v != %v", read, n)))
 	}
 
-	return data
+	return
 }
 
-func parseXBytes(buf *bufio.Reader, n int) uint32 {
-	parsed := getXBytes(buf, n)
+func parseXBytes(buf *bufio.Reader, n int) (res uint32, rerr error) {
+	parsed, err := getXBytes(buf, n)
+	if err != nil {
+		rerr = err
+		return
+	}
 
 	for len(parsed) < 4 {
 		parsed = append(parsed, 0x0)
 	}
 
-	return uint32(binary.BigEndian.Uint16(parsed))
+	res = binary.BigEndian.Uint32(parsed)
+	return
 }
 
 func atEOF(buf *bufio.Reader) bool {
 	eofSignature := []byte{'E', 'O', 'F'}
 	next3Bytes, err := buf.Peek(3)
 	if err != nil {
+		if err == io.EOF {
+			return true
+		}
 		panic(err)
 	}
 
@@ -60,7 +69,7 @@ func FromFile(fileName string) (res []Record) {
 
 	buf := bufio.NewReader(handle)
 
-	header := getXBytes(buf, 5)
+	header, _ := getXBytes(buf, 5)
 	err = ips.ValidateHeader(&header)
 	if err != nil {
 		panic(err)
@@ -69,21 +78,33 @@ func FromFile(fileName string) (res []Record) {
 	for !atEOF(buf) {
 		res = append(res, parseRecord(buf))
 	}
-
 	return
 }
 
 func parseRecord(buf *bufio.Reader) (res Record) {
-	res.Offset = parseXBytes(buf, 3)
-	res.Size = uint16(parseXBytes(buf, 2))
+	offset, err := parseXBytes(buf, 3)
+	if err != nil {
+		panic(err)
+	}
+	res.Offset = offset
+
+	size, err := parseXBytes(buf, 2)
+	if err != nil {
+		panic(err)
+	}
+	res.Size = uint16(size)
 
 	if res.Size == 0 {
 		res.IsRLE = true
 
-		res.RLESize = uint16(parseXBytes(buf, 2))
-		res.rawValue = getXBytes(buf, 1)
+		rleSize, _ := parseXBytes(buf, 2)
+		res.RLESize = uint16(rleSize)
+
+		rawVal, _ := getXBytes(buf, 1)
+		res.rawValue = rawVal
 	} else {
-		res.rawValue = getXBytes(buf, int(res.Size))
+		rawVal, _ := getXBytes(buf, int(res.Size))
+		res.rawValue = rawVal
 	}
 
 	return
